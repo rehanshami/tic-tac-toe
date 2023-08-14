85893e3d4b6e8474e7b0f8477d9371654ea8be6b
rehanshami <77270784+rehanshami@users.noreply.github.com>
2023-08-14 19:46:04 +1200

    Improve user experience by handling edge cases
    - Add condition to prompt user to click start game before making a move
    - Prevent error when activePlayer is undefined
    - Improve overall user experience and prevent confusion

--- script.js
+++ script.js
@@ -22,7 +22,6 @@ const Player = (name, marker) => {
     return { name, marker };
 }
 
-
 // Module that creates an empty gameboard
 const gameboard = (() => {
     const columns = 3;
@@ -32,11 +31,8 @@ const gameboard = (() => {
         board.push("");
     }
     console.log(board)
-
-
      // display square for each array item
      let squares = document.querySelector('.squares');
-
      board.forEach((item, index) => {
         const square = document.createElement('div');
         square.className = 'square';
@@ -44,31 +40,73 @@ const gameboard = (() => {
      })
 
      Array.from(squares.children).forEach((square, index) =>{
-        square.addEventListener('click',() => {
-            if (gameboard.board[index]==="") {
-                gameboard.board[index] = game.activePlayer.marker;
-                square.textContent = game.activePlayer.marker;
-                game.checkWinner();
-                game.switchPlayer();
-                game.checkTie();
-                console.log(game.activePlayer)
-            }
-            console.log(gameboard.board[index])
-        })
-     })
+            square.addEventListener('click',() => {
+                if (game.gameOver) {
+                    return;
+                } 
+                else if (game.activePlayer === undefined) {
+                    game.subtext.textContent = "No player names entered. Enter a player name and click Start game"
+                }
+                else if (game.activePlayer.name==="") {
+                    game.subtext.textContent = "No player names entered. Enter a player name to begin."
+                    return;
+                }
+
+                
+                else if (gameboard.board[index]==="") {
+                    gameboard.board[index] = game.activePlayer.marker;
+                    square.textContent = game.activePlayer.marker;
+                    game.checkWinner();
+                    if (game.gameOver) {
+                        return;
+                    }
+                    game.switchPlayer();
+                    game.moveInformer();
+                    game.checkTie();
+                    console.log(game.activePlayer)
+                }
+                console.log(gameboard.board[index])
+            })
+        });
 
      return {
         board
     };
-
 })();
 
 const game = (() => {
-    const player1 = Player('taco', 'X');
-    const player2 = Player('many', 'O');
-
-    let activePlayer = player2;
-
+    let player1;
+    let player2;
+    let gameOver = false;
+    let subtext = document.querySelector('.subtext');
+    let activePlayer;
+    function start() {
+        if (player1.name==="" && player2.name==="") {
+            console.log('No players entered')
+            subtext.textContent = "No player names entered. Enter a name for at least 1 player.";
+        }
+        else if (player1.name==="" && player2.name.length >0) {
+            player1.name= "player1";
+        } else if (player2.name==="" && player1.name.length > 0) {
+            player2.name="player2";
+        }
+        console.log({player1, player2})
+        activePlayer = player2;
+        moveInformer()
+    }
+    function collectUserInformation() {
+        const submitButton = document.querySelector('.submit');
+        const player1Input = document.querySelector('#player1');
+        const player2Input = document.querySelector('#player2');
+        submitButton.addEventListener('click', (event)=>{
+            event.preventDefault();
+            player1 = Player(player1Input.value, 'X');
+            player2 = Player(player2Input.value, 'O');
+            start();
+            console.log({player1, player2});
+        })
+    }
+    collectUserInformation();
     const winningAxes = [
         [0, 1, 2],
         [3, 4, 5],
@@ -80,10 +118,15 @@ const game = (() => {
         [2, 4, 6],
 
     ];
-
-
-
+    function moveInformer() {
+        if (player1.name.length > 0 || player2.name.length > 0) {
+            subtext.textContent = `${activePlayer.name}, make your move`;
+        }
+    }
     function checkWinner() {
+        if (game.gameOver) {
+            return;
+        }
         for (let axes of winningAxes) {
             console.log(axes);
             const [a, b, c] = axes;
@@ -92,12 +135,13 @@ const game = (() => {
                 gameboard.board[b]===activePlayer.marker &&
                 gameboard.board[c]===activePlayer.marker
             ) {
-                console.log(`winner is ${activePlayer.name}`)
+                console.log(`winner is ${game.activePlayer.name}`)
+                subtext.textContent = `Winner of the game is ${game.activePlayer.name}`;
+                gameOver = true;
                 return;
             }
         }
     }
-
     function switchPlayer() {
         if (activePlayer === player1) {
             activePlayer = player2;
@@ -105,19 +149,21 @@ const game = (() => {
             activePlayer = player1;
         }
     }
-
     function checkTie() {
         if(!gameboard.board.includes("")) {
             console.log("Game is tie");
             return;
         }
     }
-
     return {
         get activePlayer() {return activePlayer},
         checkWinner,
         switchPlayer,
-        checkTie
+        checkTie,
+        get gameOver() {return gameOver},
+        start,
+        moveInformer,
+        subtext
 
     }
 })();
